e84ce6c3e8593dff5744f05aec2029b7a6c0547a
efnky <efeinkaya@gmail.com>
2026-02-10 10:25:19 +0100

    :sparkles: Created PlayerAbilitySet and NPCAbilitySet classes.

---
+++ PlayerAbilitySet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Efe İnkaya on 10/02/2026.
+//
+
+#include "PlayerAbilitySet.h"
---
+++ PlayerAbilitySet.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ICombatAbilitySet.h"
+
+class Character;
+class Player;
+
+class PlayerAbilitySet : public ICombatAbilitySet {
+public:
+
+};
