c529d37c0fa4bb1eb3a7d01342c8d1c02f63871d
efnky <efeinkaya@gmail.com>
2026-02-10 09:42:00 +0100

    :sparkles: Created an interface for combat ability set called ICombatAbilitySet.

---
+++ ICombatAbilitySet.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Character;
+
+class ICombatAbilitySet {
+public:
+    virtual ~ICombatAbilitySet() = default;
+
+    // Attacking
+
+    // Defending
+
+    // Healing
+};
