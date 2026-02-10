dd2bd25475c95c69de56c92141d5521ed2de29aa
efnky <efeinkaya@gmail.com>
2026-02-10 10:11:11 +0100

    :hammer: Updated the interface ICombatAbilitySet.

--- ICombatAbilitySet.h
+++ ICombatAbilitySet.h
@@ -8,8 +8,15 @@ public:
     virtual ~ICombatAbilitySet() = default;
 
     // Attacking
+    virtual void meleeAttack(Character& other) = 0;
+    virtual void weaponAttack(Character& other) = 0;
+    virtual void specialAttack(Character& other) = 0;
 
     // Defending
+    virtual void block(Character& other) = 0;
+    virtual void dodge(Character& other) = 0;
 
     // Healing
+    virtual void usePotion() = 0;
+    virtual void heal() = 0;
 };
